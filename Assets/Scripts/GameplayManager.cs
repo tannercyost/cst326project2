80848a534419ab59de34284f5011dabec832b61b
tannercyost <42818307+tannercyost@users.noreply.github.com>
2020-02-16 18:43:39 -0800

    Finished time UI element

---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayManager : MonoBehaviour
+{
+    public TextMeshProUGUI timeText;
+    public TextMeshProUGUI scoreText;
+
+    public float countdown = 400;
+    private int score = 0;
+    private int time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        timeText.SetText("TIME \n" + (int) countdown);
+    }
+}
